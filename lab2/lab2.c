beb287decef40e7fa338d7bd016642408c5185cf
up201505648 <up201505648@db8ca917-da49-4bcd-8bc5-abb2cdec74d5>
2016-10-07 12:59:42 +0000

    Teste cica

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 		print_usage(argv);
 		return 0;
 	}
-	else return proc_args(argc, argv); /* teste*/
+	else return proc_args(argc, argv); /* teste2*/
 }
 
 static void print_usage(char **argv)
