39a9c4d2f1a0b6a04116e7f0e2186f7c158c5200
João Seixas <myownxdeath@users.noreply.github.com>
2016-10-28 10:02:30 +0000

    Updated by up201505648

--- lab3/keyboard.h
+++ lab3/keyboard.h
@@ -12,6 +12,9 @@ int kbd_subscribe_int(void);
 int kbd_unsubscribe_int();
 int kbd_make_or_break(unsigned long data);
 unsigned long kbd_handler();
+int kbd_ACK(unsigned long cmd);
+int kbd_led_handler(unsigned long cmd1, unsigned long cmd2) ;
+int kbd_send_command(unsigned long cmd);
 
 
 #endif /* __KEYBOARD_H */
--- lab3/test3.c
+++ lab3/test3.c
@@ -13,75 +13,67 @@ void timer_int_handler() {
 
 int kbd_test_scan(unsigned short ass) {
 
-		int r, ipc_status, irq_set, counter = 0;
-		message msg;
+	int r, ipc_status, irq_set, counter = 0;
+	message msg;
 
-		irq_set = kbd_subscribe_int();
-		if (irq_set == -1) {
-			printf("Error in kbd_subscribe_int()\n");
-			return 1;
+	irq_set = kbd_subscribe_int();
+	if (irq_set == -1) {
+		printf("Error in kbd_subscribe_int()\n");
+		return 1;
+	}
+	unsigned long data;
+	unsigned int bool = 0;
+
+	while (data != ESC_BREAK) {
+
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) { /*Get a request message.*/
+			printf("driver_receive failed with: %d", r);
+			continue;
 		}
-		unsigned long data, data2;
-		unsigned int bool = 0;
-
-		while (data != ESC_BREAK) {
-
-			if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) { /*Get a request message.*/
-				printf("driver_receive failed with: %d", r);
-				continue;
-			}
-			if (is_ipc_notify(ipc_status)) { /*received notification*/
-				switch (_ENDPOINT_P(msg.m_source)) {
-				case HARDWARE: /*hardware interrupt notification*/
-					if (msg.NOTIFY_ARG & irq_set) /*subscribed interrupt*/
-						if (ass == 0)
-						{
-							data = kbd_handler();  /*process it*/
+		if (is_ipc_notify(ipc_status)) { /*received notification*/
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /*hardware interrupt notification*/
+				if (msg.NOTIFY_ARG & irq_set) { /*subscribed interrupt*/
+					if (ass == 0)
+						data = kbd_handler(); /*process it*/
+					else
+						data = assembly();
+					if (data == -1)
+						return 1;
+					if (data == TWO_BYTES || bool) {
+						if (!bool) {
+							bool = 1;
+						} else {
+							if (kbd_make_or_break(data))
+								printf("Breakcode: 0xE0%02X\n", data);
+							else
+								printf("Makecode: 0xE0%02X\n", data);
+							bool = 0;
 						}
-						else if (ass==1)
-						{   data = assembly(); }
-							if (data == -1)
-								return 1;
-							if (data == TWO_BYTES || bool) {
-								if (!bool) {
-									bool = 1;
-								} else {
-									if (kbd_make_or_break(data))
-										printf("Breakcode: 0xE0%X\n", data);
-									else
-										printf("Makecode: 0xE0%X\n", data);
-									bool = 0;
-								}
 
-							} else {
-								if (kbd_make_or_break(data))
-									printf("Breakcode: 0x%X\n", data);
-								else
-									printf("Makecode: 0x%X\n", data);
-							}
+					} else {
+						if (kbd_make_or_break(data))
+							printf("Breakcode: 0x%02X\n", data);
+						else
+							printf("Makecode: 0x%02X\n", data);
+					}
 
+					break;
+				}
+			default:
+				break;
+			}/*no other notifications expected: do nothing*/
 
-						break;
-					default:
-						break; }/*no other notifications expected: do nothing*/
-
-				   }else { /*received a    standard message, not a notification*/
-					/*
-					 no standard messages expected: do nothing
-					 */}}}
-
-
-
-
-
-
-
-
+		} else { /*received a    standard message, not a notification*/
+			/*
+			 no standard messages expected: do nothing
+			 */}
+	}
+}
 
 int kbd_test_leds(unsigned short n, unsigned short *leds) {
-	unsigned long counter = 0;
-	int r, ipc_status;
-	unsigned long irq_set;
+	counter = 0;
+	int r, ipc_status, irq_set;
 	message msg;
 	kbd_subscribe_int();
 	irq_set = timer_subscribe_int();
@@ -105,13 +97,11 @@ int kbd_test_leds(unsigned short n, unsigned short *leds) {
 			case HARDWARE: /*hardware interrupt notification*/
 				if (msg.NOTIFY_ARG & irq_set) { /*subscribed interrupt*/
 					timer_int_handler(); /*process it*/
-					printf(".\n");
 					if (counter % 60 == 0) {
 						printf("Entrou\n");
-
 						if (kbd_led_handler(KB_LED_CMD, BIT(leds[counter / n]))
-								!= OK){
-						printf("Merdou\n");
+								!= OK) {
+							printf("Saiu com erro\n");
 							//return -1;
 
 						}
@@ -152,7 +142,7 @@ int kbd_test_timed_scan(unsigned short n) {
 		return 1;
 	}
 
-	unsigned long data, data2;
+	unsigned long data;
 	unsigned int bool = 0;
 
 	while ((counter / 60 < n) && (data != ESC_BREAK)) {
@@ -176,17 +166,17 @@ int kbd_test_timed_scan(unsigned short n) {
 							bool = 1;
 						} else {
 							if (kbd_make_or_break(data))
-								printf("Breakcode: 0xE0%X\n", data);
+								printf("Breakcode: 0xE0%02X\n", data);
 							else
-								printf("Makecode: 0xE0%X\n", data);
+								printf("Makecode: 0xE0%02X\n", data);
 							bool = 0;
 						}
 
 					} else {
 						if (kbd_make_or_break(data))
-							printf("Breakcode: 0x%X\n", data);
+							printf("Breakcode: 0x%02X\n", data);
 						else
-							printf("Makecode: 0x%X\n", data);
+							printf("Makecode: 0x%02X\n", data);
 					}
 					counter = 0;
 				}
